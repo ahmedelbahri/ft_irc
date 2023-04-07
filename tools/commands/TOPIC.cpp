ab04cd8ee4767da7b498b42c17a9d11680b088aa
Ahmed El bahri <ahel-bah@e1r5p4.1337.ma>
2023-04-07 14:11:06 +0000

    edit invite cmd if the channel not in mode invite_only

--- tools/commands/TOPIC.cpp
+++ tools/commands/TOPIC.cpp
@@ -16,14 +16,7 @@ void	irc_client::TOPIC(std::string args)
 		else if (!check_if_user_in_channel(this->fd, chan))
 			send_error(this->fd, ":" + this->nick + " 442 " + nick + " :you are not on that channel\n");
 		else if (args.empty())
-		{
-			std::time_t timestamp = std::time(nullptr);
-			std::tm* timeinfo = std::localtime(&timestamp);
-			char buffer[80];
-			std::strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
 			send_error(fd, ":" + clients[fd].get_nick() + (channels[chan].get_topic().empty() ? "331 TOPIC :No topic is set\n" : "332 TOPIC :" + channels[chan].get_topic() + "\n"));
-			// send_error(fd, ":" + clients[fd].get_nick() + " 333 " + chan + " " + buffer + "\n");
-		}
 		else if (!args.empty() && ((channels[chan].get_topic_settable_by_op() && isElementInVector(channels[chan].get_opp() ,this->fd)) || !channels[chan].get_topic_settable_by_op()))
 		{
 			std::string	topic = args.substr(args.find_first_not_of(" ", nick.size()), args.find_last_not_of(" ") + 1);
