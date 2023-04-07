2eb8eb0e89565e3b5b4f958fc072d62ada8219b4
Asmaa Elabid <aelabid@e1r6p3.1337.ma>
2023-04-07 17:21:42 +0000

    edit delete_client function

--- tools/delete_client.cpp
+++ tools/delete_client.cpp
@@ -1,10 +1,13 @@
 #include "../includes/ft_irc.hpp"
 
+
 void	delete_client(int fd)
 {
 	std::map<std::string, irc_channel>::iterator	it_channels;
 	for (it_channels = channels.begin(); it_channels != channels.end(); it_channels++)
 	{
+		if (check_if_user_in_channel(fd, it_channels->first))
+			send_error(fd, ":" + clients[fd].get_nick() + " PART " + it_channels->first);
 		eraseElementFromVector(it_channels->second.get_members(), fd);
 		eraseElementFromVector(it_channels->second.get_invites(), fd);
 		eraseElementFromVector(it_channels->second.get_opp(), fd);
